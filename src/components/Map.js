f5e58b53a736fc1c4dc9d53959ce1386aa4b0eef
Aaryaman Mehta <aaryaman.s.m@gmail.com>
2023-01-03 00:58:42 +0530

    pan to chosen location, create route, show distance and duration

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GoogleMap, LoadScript, Autocomplete } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, StandaloneSearchBox, DirectionsRenderer } from '@react-google-maps/api';
 import './Map.css'
 
 const containerStyle = {
@@ -24,7 +24,37 @@ const options = {
 };
 
 function Map() {
-    const [map, setMap] = React.useState(/** @type google.maps.Map  */ (null))       
+    const [map, setMap] = React.useState(/** @type google.maps.Map */ (null))
+    const [origin, setOrigin] = React.useState(null)
+    const onOriginLoad = ref => {
+        setOrigin(ref);
+      };
+    const [destination, setDestination] = React.useState(null)
+    const onDestinationLoad = ref => {
+        setDestination(ref);
+    };
+    const [directionsService, setDirectionsService] = React.useState(null) 
+    const [distance, setDistance] = React.useState('')
+    const [duration, setDuration] = React.useState('')
+    /** @type React.MutableRefObject<HTMLInputElement> */
+    const originRef = React.useRef()
+    /** @type React.MutableRefObject<HTMLInputElement> */
+    const destinationRef = React.useRef()
+    async function calculateRoute() {
+        if (originRef.current.value === '' || destinationRef.current.value === '') {
+          return
+        }
+        const directionsService = new window.google.maps.DirectionsService()
+        const results = await directionsService.route({
+          origin: originRef.current.value,
+          destination: destinationRef.current.value,
+          travelMode: window.google.maps.TravelMode.DRIVING,
+        })
+        setDirectionsService(results)
+        setDistance(results.routes[0].legs[0].distance.text)
+        setDuration(results.routes[0].legs[0].duration.text)
+    }
+
     return (
         <LoadScript
             googleMapsApiKey="AIzaSyDCdzSKDsuKcjUUh8DwL7lB_TOKWn6AHIc"
@@ -37,10 +67,14 @@ function Map() {
             options = {options}
             onLoad={map => setMap(map)}
         >
-        <Autocomplete>
+        {directionsService && <DirectionsRenderer directions={directionsService} />}  
+        <StandaloneSearchBox
+            onLoad={onOriginLoad}
+        >
             <input
                 type="text"
                 placeholder="Enter your origin"
+                ref={originRef}
                 style={{
                     border: `1px solid black`,
                     width: `240px`,
@@ -57,7 +91,7 @@ function Map() {
                     top: "2.5%",
                 }}
             />
-        </Autocomplete>
+        </StandaloneSearchBox>
             <input
                 type="button"
                 value="↪"
@@ -74,9 +108,96 @@ function Map() {
                     left: "21.5%",
                     top: "2.5%",
                 }}
-                onClick = {() => map.panTo(center)}
+                onClick = {() => map.panTo(origin.getPlaces()[0].geometry.location)}
             />
-          </GoogleMap>
+        <StandaloneSearchBox
+            onLoad={onDestinationLoad}
+        >
+            <input
+                type="text"
+                placeholder="Enter your final destination"
+                ref={destinationRef}
+                style={{
+                    border: `1px solid black`,
+                    width: `240px`,
+                    height: `32px`,
+                    padding: `0 12px`,
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `14px`,
+                    color: '#333333',
+                    fontFamily: 'Helvetica',
+                    outline: `none`,
+                    textOverflow: `ellipses`,
+                    position: "absolute",
+                    left: "1.5%",
+                    top: "9.5%",
+                }}
+            />
+        </StandaloneSearchBox>
+            <input
+                type="button"
+                value="↪"
+                style={{
+                    width: `32px`,
+                    height: `34px`,
+                    paddingTop: '3.5px',
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `15px`,
+                    color: '#333333',
+                    fontFamily: 'Helvetica',
+                    textOverflow: `ellipses`,
+                    position: "absolute",
+                    left: "21.5%",
+                    top: "9.5%",
+                }}
+                onClick = {() => map.panTo(destination.getPlaces()[0].geometry.location)}
+            />
+            <input
+                type="button"
+                value="Calculate Route"
+                style={{
+                    width: '267px',
+                    height: `34px`,
+                    paddingTop: '3.5px',
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `15px`,
+                    color: '#333333',
+                    fontFamily: 'Helvetica',
+                    textOverflow: `ellipses`,
+                    position: "absolute",
+                    left: "1.5%",
+                    top: "16.5%",
+                }}
+                onClick = {function(event) { calculateRoute();}}
+            />
+            <div>
+                <p style={{                
+                    width: '247px',
+                    height: `50px`,
+                    padding: '9.5px',
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                    border: `1px solid black`,
+                    fontSize: `15px`,
+                    color: '#333333',
+                    fontFamily: 'Helvetica',
+                    textOverflow: `ellipses`,
+                    position: "absolute",
+                    left: "1.5%",
+                    top: "22%",
+                }}>Distance: {distance}</p>
+                <p style={{
+                    padding: '10.5px',
+                    fontSize: `15px`,
+                    color: '#333333',
+                    fontFamily: 'Helvetica',
+                    textOverflow: `ellipses`,
+                    position: "absolute",
+                    left: "1.5%",
+                    top: "26.7%",
+                }}>Duration: {duration}</p>
+            </div>
+        </GoogleMap>
         </LoadScript>
     )
 }
